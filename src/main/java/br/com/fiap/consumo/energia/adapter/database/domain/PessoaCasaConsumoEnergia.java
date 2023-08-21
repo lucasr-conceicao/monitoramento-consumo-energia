f52476de42c3f9d1b4ee18215c495529552a188a
Lucas Rocha <lucasr-conceicao@outlook.com.br>
2023-08-21 00:13:53 -0300

    Mapeamento inicial das classes que representam o banco de dados

---
+++ src/main/java/br/com/fiap/consumo/energia/adapter/database/domain/PessoaCasaConsumoEnergia.java
@@ -0,0 +1,16 @@
+package br.com.fiap.consumo.energia.adapter.database.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.io.Serializable;
+
+@Getter
+@Builder
+@Entity
+@Table(name = "")
+public class PessoaCasaConsumoEnergia implements Serializable {
+
+}
---
+++ src/main/java/br/com/fiap/consumo/energia/adapter/database/domain/TipoCasaConsumoEnergia.java
@@ -0,0 +1,16 @@
+package br.com.fiap.consumo.energia.adapter.database.domain;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Getter
+@Builder
+@Entity
+@Table(name = "")
+public class TipoCasaConsumoEnergia implements Serializable {
+
+}
